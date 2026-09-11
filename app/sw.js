0c114bd19b4ac9dc3ccbd05ac0680220103c9fb9
nekakoval-crypto <nekakoval@gmail.com>
2026-09-11 21:49:46 +0300

    Stage74G cache installable PWA shell

--- app/sw.js
+++ app/sw.js
@@ -1,5 +1,5 @@
-const CACHE='pbk-shell-v2';
-const SHELL=['/','/index.html','/app.css','/filters.css','/app.js','/filters.js','/manifest.webmanifest'];
+const CACHE='pbk-shell-v3';
+const SHELL=['/','/index.html','/app.css','/filters.css','/install.css','/app.js','/filters.js','/install.js','/icon.svg','/manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('message',e=>{if(e.data?.type==='SKIP_WAITING')self.skipWaiting()});
